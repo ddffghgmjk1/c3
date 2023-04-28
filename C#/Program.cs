bc08b371bf92110782dc66c64108bef494980da7
ddffghgmjk1 <132074262+ddffghgmjk1@users.noreply.github.com>
2023-04-28 17:56:19 +0300

    Add files via upload

---
+++ C#/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+//4 -> да
+//-3 -> нет
+//7 -> нет
+
+System.Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(a/2);
+Console.WriteLine(a%2);
